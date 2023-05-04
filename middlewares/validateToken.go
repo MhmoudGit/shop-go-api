b269d0bb93cf119e7ce7a71263ddc025f4bd88f1
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2023-05-04 19:52:09 +0300

    validate

--- middlewares/validateToken.go
+++ middlewares/validateToken.go
@@ -9,6 +9,23 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
+func decrypt(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("unexpected signing method")
+	}
+	return []byte(config.JWTSecret), nil
+}
+
+func checkToken(token string, w http.ResponseWriter) {
+	if token == "" {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("Invalid Credentials"))
+		return
+	}
+	token = strings.Replace(token, "Bearer ", "", 1)
+	return
+}
+
 func ValidateMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		tokenString := r.Header.Get("Authorization")
@@ -18,12 +35,7 @@ func ValidateMiddleware(next http.HandlerFunc) http.HandlerFunc {
 			return
 		}
 		tokenString = strings.Replace(tokenString, "Bearer ", "", 1)
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, fmt.Errorf("unexpected signing method")
-			}
-			return []byte(config.JWTSecret), nil
-		})
+		token, err := jwt.Parse(tokenString, decrypt)
 		if err != nil {
 			w.WriteHeader(http.StatusUnauthorized)
 			w.Write([]byte(err.Error()))
